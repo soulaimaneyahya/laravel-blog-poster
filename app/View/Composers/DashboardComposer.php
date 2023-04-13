ef7c03376b9f70ce64c1b06dc0a8f59a948f1374
Soulaimane yahya <soulaimaneyh07@gmail.com>
2023-04-13 19:54:13 +0000

    feat - posts-data-service

--- app/View/Composers/DashboardComposer.php
+++ app/View/Composers/DashboardComposer.php
@@ -2,10 +2,17 @@
 
 namespace App\View\Composers;
 
+use App\Services\PostsStatsData;
 use Illuminate\View\View;
 
 class DashboardComposer
 {
+    public function __construct(
+        private PostsStatsData $postsStatsData
+    ) {
+      //  
+    }
+
     public function compose(View $view)
     {
         // dashboard data
@@ -17,35 +24,6 @@ class DashboardComposer
         $postsCountTrashed = auth()->user()->posts()->onlyTrashed()->get()->count();
         $postsCountTrashedPercent =  round(($totalPosts > 0) ? ($postsCountTrashed * 100) / $totalPosts : 0, 2) . '%';
 
-        $current_date = date('Y-m-d');
-        // $last_15_days_date = date('Y-m-d', strtotime('-15 days'));
-
-        // Create an empty array to hold the dates
-        $postsStatsData = [];
-        // Get the current date
-        $current_date = strtotime(date('Y-m-d'));
-        // Loop through the last 15 days
-        for ($i = 0; $i < 15; $i++) {
-            // Get the date for the current iteration
-            $date = strtotime("-$i day", $current_date);
-            // Add the date to the array in the desired format
-            $count = auth()->user()->recievedLikes()->whereDate('likes.created_at', '=', date('Y-m-d', $date))->get()->count();
-            $formattedDate = date('F j', $date);
-
-            // $postsStatsData[] = $formattedDate;
-            $postsStatsData[$formattedDate] = $count;
-        }
-
-        // Filter out the dates with count <= 0
-        // $postsStatsData = array_filter($postsStatsData, function($value, $key) {
-        //     return $value > 0;
-        // }, ARRAY_FILTER_USE_BOTH);
-
-        // dd($postsStatsData);
-
-        // Reverse the array so that the dates are in chronological order
-        $postsStatsData = array_reverse($postsStatsData, true);
-        // end dashboard data
     
         $view->with('totalPosts', $totalPosts);
 
@@ -55,6 +33,6 @@ class DashboardComposer
         $view->with('postsCountTrashed', $postsCountTrashed);
         $view->with('postsCountTrashedPercent', $postsCountTrashedPercent);
 
-        $view->with('postsStatsData', $postsStatsData);
+        $view->with('postsStatsData', $this->postsStatsData->data());
     }
 }
